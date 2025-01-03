715d26a7ea6e491fffa034658a68d13700610d6a
Durrani27229 <u87262@gmail.com>
2025-01-04 03:10:35 +0500

    16 teeth

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const physicalButton = () => {
+    const physicalBtn = document.getElementById("physical-btn");
+    physicalBtn.style.backgroundColor = "#000000";
+    physicalBtn.style.color = "#ffffff";
+    const onlineBtn = document.getElementById("online-btn");
+    onlineBtn.style.backgroundColor = "#ffffff";
+    onlineBtn.style.color = "#000000";
+}
+
+const onlineButton = () => {
+    const onlineBtn = document.getElementById("online-btn");
+    onlineBtn.style.backgroundColor = "#000000";
+    onlineBtn.style.color = "#ffffff";
+    const physicalBtn = document.getElementById("physical-btn");
+    physicalBtn.style.backgroundColor = "#ffffff";
+    physicalBtn.style.color = "#000000";
+}
+
+
+
