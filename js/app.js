45c597555bde5845c4a87e991354920342cbfe74
Durrani27229 <u87262@gmail.com>
2025-01-16 01:46:27 +0500

    16-jan

--- js/app.js
+++ js/app.js
@@ -18,3 +18,23 @@ const onlineButton = () => {
 
 
 
+const personalDetailBtn = () => {
+    const personalDetail = document.getElementById("btn-personal");
+    personalDetail.style.backgroundColor = "#000000";
+    personalDetail.style.color = "#ffffff";
+    const bookingDetail = document.getElementById("btn-booking");
+    bookingDetail.style.backgroundColor = "#ffffff";
+    bookingDetail.style.color = "#000000";
+    const disputeDetail = document.getElementById("btn-dispute");
+    disputeDetail.style.backgroundColor = "#ffffff";
+    disputeDetail.style.color = "#000000";
+    const reviewsDetail = document.getElementById("btn-reviews");
+    reviewsDetail.style.backgroundColor = "#ffffff";
+    reviewsDetail.style.color = "#000000";
+    const clientsDetail = document.getElementById("btn-clients");
+    clientsDetail.style.backgroundColor = "#ffffff";
+    clientsDetail.style.color = "#000000";
+    const earningsDetail = document.getElementById("btn-earnings");
+    earningsDetail.style.backgroundColor = "#ffffff";
+    earningsDetail.style.color = "#000000";
+}
