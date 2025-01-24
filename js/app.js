314ab2f52b68595266b4fda9c272c3452fe8a3a2
Durrani27229 <u87262@gmail.com>
2025-01-25 03:33:43 +0500

    25-Jan

--- js/app.js
+++ js/app.js
@@ -18,23 +18,13 @@ const onlineButton = () => {
 
 
 
-const personalDetailBtn = () => {
-    const personalDetail = document.getElementById("btn-personal");
-    personalDetail.style.backgroundColor = "#000000";
-    personalDetail.style.color = "#ffffff";
-    const bookingDetail = document.getElementById("btn-booking");
-    bookingDetail.style.backgroundColor = "#ffffff";
-    bookingDetail.style.color = "#000000";
-    const disputeDetail = document.getElementById("btn-dispute");
-    disputeDetail.style.backgroundColor = "#ffffff";
-    disputeDetail.style.color = "#000000";
-    const reviewsDetail = document.getElementById("btn-reviews");
-    reviewsDetail.style.backgroundColor = "#ffffff";
-    reviewsDetail.style.color = "#000000";
-    const clientsDetail = document.getElementById("btn-clients");
-    clientsDetail.style.backgroundColor = "#ffffff";
-    clientsDetail.style.color = "#000000";
-    const earningsDetail = document.getElementById("btn-earnings");
-    earningsDetail.style.backgroundColor = "#ffffff";
-    earningsDetail.style.color = "#000000";
-}
+
+// const editBtn = document.getElementById("edit");
+// console.log(editBtn);
+
+
+
+// editBtn.addEventListener("click", () => {
+//     const editCart = document.getElementById("edit");
+//     editCart.style.display = "block";
+// })
